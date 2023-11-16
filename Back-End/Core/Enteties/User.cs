130bf31bd217deba915fff689e96280dbcb902df
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-11-16 23:57:09 +0100

    Adding some logic to the controller

--- Back-End/Core/Enteties/User.cs
+++ Back-End/Core/Enteties/User.cs
@@ -2,6 +2,9 @@ namespace Core.Enteties
 {
     public class User : BaseEntity
     {
-          
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ShortDescription { get; set; }
     }
 }
--- Back-End/api/Controllers/UsersController.cs
+++ Back-End/api/Controllers/UsersController.cs
@@ -2,14 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.TransferModels;
 using Microsoft.AspNetCore.Mvc;
+using Service;
 
 namespace api.Controllers
 {
     [ApiController]
     public class UsersController : ControllerBase
     {
-        
+        private readonly UserService _userService;
+
+        public UsersController (UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("/api/users")]
+        public ResponseDto GetAllUsers()
+        {
+            return new ResponseDto()
+            {
+                MessageToClient = "Here are all the users!"
+            };
+        }
         
     }
 }
