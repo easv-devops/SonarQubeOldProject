a3d00721764c68c1f82b8ecd3f1d08287fd28222
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-11-17 00:08:25 +0100

    Fixed env var

--- Back-End/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ Back-End/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130bf31bd217deba915fff689e96280dbcb902df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b3db54a1e13bb986437b887ac9ebb6e087776f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
