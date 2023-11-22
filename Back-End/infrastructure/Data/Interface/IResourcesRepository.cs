112b71786c3d0e3a5b99f9648248972858dc4ae5
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-11-22 17:30:23 +0100

    Added all the IRepositories

---
+++ Back-End/infrastructure/Data/Interface/IResourcesRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Enteties;
+
+namespace infrastructure.Data.Interface
+{
+    public interface IResourcesRepository
+    {
+        IEnumerable<Resources> GetAll();
+        Resources GetById(int id);
+        Resources Create(string name, string type, string link, int courseId);
+        Resources Update(int id, string name, string type, string link, int courseId);
+    }
+}
