af50fd199b602c9c771e29836ac95b7e6d5a3a7c
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-12-21 01:21:51 +0100

    deployment succsefull

--- Back-End/infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
+++ Back-End/infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3214357c10038790eedf8f2bf88fac540d35f9c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16edb273e94c580cdbffc80ec341811b3ab6253e")]
 [assembly: System.Reflection.AssemblyProductAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
