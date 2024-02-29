46761674437907f4d6dea46dc6e507891b1025cf
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-02-29 20:34:53 +0100

    aded

--- Back-End/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Back-End/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9814ade3ff6dd50436ef941a3bb107e885b0747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e160fbc5eb4b109d64f1ece2a15bb30558bffdbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
